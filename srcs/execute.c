87e4e58913d26285852d4680136cee019db67276
Kyle Bamping <kylebamping7@gmail.com>
2016-08-27 02:33:30 +0200

    Update to setenv and unsetenv

--- srcs/execute.c
+++ srcs/execute.c
@@ -6,7 +6,7 @@
 /*   By: kbamping <kbamping@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/09 01:25:24 by kbamping          #+#    #+#             */
-/*   Updated: 2016/08/27 01:13:47 by kbamping         ###   ########.fr       */
+/*   Updated: 2016/08/27 02:14:23 by kbamping         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,15 @@
 
 static int	try_parent_builtin(t_shell *s)
 {
+	int		ret;
+	char	*tmp;
+
 	if (ft_strcmp(s->input[0], "cd") == 0)
 		return (ft_cd(s->input, s));
 	else if (ft_strcmp(s->input[0], "exit") == 0)
 	   return (EXIT_SH);
 //	else if (ft_strcmp(s->input[0], "set") == 0 && !s->input[1])
-//	{
 //		return (ft_set(2, NULL, NULL, s));
-//	}
-	else
-		return (ERR_NOTFOUND);
-}
-
-static int	try_child_builtin(t_shell *s)
-{
-	int		ret;
-	char	*tmp;
-
-	if (ft_strcmp(s->input[0], "echo") == 0)
-		return (ft_echo(s->input, s));
-	else if (ft_strcmp(s->input[0], "env") == 0)
-		return (ft_env(s->input, s));
-//	else if (ft_strcmp(s->input[0], "set") == 0)
-//		return (ft_set(2, s->input[1], s->input[1], s));
 	else if (ft_strcmp(s->input[0], "setenv") == 0)
 		return (ft_set(1, s->input[1], s->input[2], s));
 	else if (ft_strcmp(s->input[0], "export") == 0)
@@ -48,9 +34,32 @@ static int	try_child_builtin(t_shell *s)
 	}
 	else if (ft_strcmp(s->input[0], "unsetenv") == 0)
 		return (ft_unsetenv(s->input[1], s));
+	else
+		return (ERR_NOTFOUND);
+}
+
+static int	try_child_builtin(t_shell *s)
+{
+	if (ft_strcmp(s->input[0], "echo") == 0)
+		return (ft_echo(s->input, s));
+	else if (ft_strcmp(s->input[0], "env") == 0)
+		return (ft_env(s->input, s));
+//	else if (ft_strcmp(s->input[0], "set") == 0)
+//		return (ft_set(2, s->input[1], s->input[1], s));
+//	else if (ft_strcmp(s->input[0], "setenv") == 0)
+//		return (ft_set(1, s->input[1], s->input[2], s));
+/*	else if (ft_strcmp(s->input[0], "export") == 0)
+	{
+		tmp = ft_strdup(ft_getenv(s->input[1], s));
+		ret = ft_set(1, s->input[1], tmp, s);
+		ft_strdel(&tmp);
+		return (ret);
+	}
+	else if (ft_strcmp(s->input[0], "unsetenv") == 0)
+		return (ft_unsetenv(s->input[1], s));
 	else if (ft_strcmp(s->input[0], "unset") == 0)
 		return (ft_unsetenv(s->input[1], s));
-	else
+*/	else
 		return (ERR_NOTFOUND);
 }
 
