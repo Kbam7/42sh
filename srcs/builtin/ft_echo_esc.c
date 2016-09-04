7e4da0667ea6479306f00ab5cde7637951ac4092
Kyle GANI <kgani@e5r10p1.jnb.42.fr>
2016-09-04 11:20:20 +0200

    line editing now works with any size prompt - few minor bugs to sort

--- srcs/builtin/ft_echo_esc.c
+++ srcs/builtin/ft_echo_esc.c
@@ -6,7 +6,7 @@
 /*   By: kgani <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/31 11:24:39 by kgani             #+#    #+#             */
-/*   Updated: 2016/09/01 09:28:32 by kgani            ###   ########.fr       */
+/*   Updated: 2016/09/04 08:56:17 by kgani            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,8 @@ int	ft_echo_esc(char **new_str, char *arg, int pos, int trigger)
 	ret = 0;
 	while (arg[pos + i] != '\0' && arg[pos + i] == '\\')
 	{
-		if (arg[pos + i + 1] != '\0')
+		if (arg[pos + i + 1] != '\0' && (trigger || arg[pos + i - 1] == '\\'))
 		{
-/*
-			if (arg[pos + i + 2] != '\0' && arg[pos + i + 2] == '\\')
-			{
-				if((ret = ft_echo_esc_char(new_str, arg[pos + i + 2])) != -1)
-					ret = 3;
-				else
-					break;
-			}
-*/
 				if ((ret = ft_echo_esc_char(new_str, arg[pos + i + 1])) != - 1 && \
 					trigger != 0 && trigger != -1)
 					ret = 2;
