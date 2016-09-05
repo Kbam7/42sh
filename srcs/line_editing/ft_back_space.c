88dd7af0639088b2f139e042a99f22aff63c0978
Tony MACK <tmack@e5r10p4.jnb.42.fr>
2016-09-05 11:09:59 +0200

    backspace update

--- srcs/line_editing/ft_back_space.c
+++ srcs/line_editing/ft_back_space.c
@@ -6,7 +6,7 @@
 /*   By: tmack <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/11 12:31:10 by tmack             #+#    #+#             */
-/*   Updated: 2016/09/04 11:37:27 by kgani            ###   ########.fr       */
+/*   Updated: 2016/09/05 11:08:48 by tmack            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,45 +36,48 @@ void	ft_line_back_space(t_shell *s)
 	s->new_line[s->str_len - 1] = '\0';
 }
 
-void    ft_put_curs_back(t_shell *s)
+void    back_shorter(t_shell *s)
 {
 	int     i;
-	int     j;
 
-	j = s->str_len + s->prompt_len;
+	i = (s->curs_pos + s->prompt_len) - (s->width * (s->cols - 1));
+	while ((i - s->width) > 0)
+		i = i - s->width;
+	if (((s->str_len + s->prompt_len - 1) - (s->width * (s->cols - 1))) ==
+			s->width && s->curs_pos == s->str_len)
+		tputs(tgetstr("do", 0), 1, ft_putchar_re);
+	if ((s->curs_col != s->cols && (s->str_len + s->prompt_len - 1) -
+				(s->width * (s->cols - 1)) > s->width))
+		tputs(tgetstr("up", 0), 1, ft_putchar_re);
+	if (i == s->width && s->curs_pos != s->str_len)
+	{
+		tputs(tgetstr("up", 0), 1, ft_putchar_re);
+		tputs(tgetstr("cr", 0), 1, ft_putchar_re);
+		i = -1;
+		while (i++ < s->width)
+			tputs(tgetstr("nd", 0), 1, ft_putchar_re);
+	}
+}
+
+void    ft_put_curs_back(t_shell *s)
+{
+	int     j;
+	int     i;
+
+	i = 0;
+	back_shorter(s);
+	j = s->curs_pos - (s->width * (s->cols - 1));
 	while (j - s->width > 0)
 		j = j - s->width;
-	if (j != s->width || s->curs_pos != s->str_len)
+	if (j + s->prompt_len != s->width)
 		tputs(tgetstr("cr", 0), 1, ft_putchar_re);
 	if (s->cols == 0)
 		i = ft_curs_pos(s) + s->prompt_len - 1;
 	else
-		i = ft_curs_pos(s) - 2;
+		i = ft_curs_pos(s) - s->prompt_len + 1;
 	while (i-- > 0)
 		tputs(tgetstr("nd", 0), 1, ft_putchar_re);
 	i = s->cols - s->curs_col;
-	while (i-- > 0 && s->str_len + s->prompt_len -
-			(s->width * (s->cols - 1)) != s->width)
-		tputs(tgetstr("up", 0), 1, ft_putchar_re);
-	if ((s->str_len + s->prompt_len - 1) == s->width )
-		tputs(tgetstr("do", 0), 1, ft_putchar_re);
-	i = ft_curs_pos(s) - 2;
-	if (s->curs_pos != s->str_len && (s->str_len + (s->prompt_len - 1)) -
-			(s->width * (s->cols - 1)) == s->width)
-		while (i-- > 0)
-			tputs(tgetstr("nd", 0), 1, ft_putchar_re);
-	i = 0;
-	if (s->curs_pos != s->str_len &&
-			(ft_curs_pos(s) == 1) && s->curs_pos != 1)
-	{
-		tputs(tgetstr("up", 0), 1, ft_putchar_re);
-		while (i++ < s->width)
-			tputs(tgetstr("nd", 0), 1, ft_putchar_re);
-	}
-	if (s->curs_pos != s->str_len &&
-			((s->str_len + (s->prompt_len - 1)) - (s->width * (s->cols - 1)) ==
-			 s->width) && s->cols != 1)
-		tputs(tgetstr("up", 0), 1, ft_putchar_re);
 }
 
 void	ft_back_clear(t_shell *s)
