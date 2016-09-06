06e0541b3abf71e9982e36ec560c500aa8d45abf
Richard BROMILOW <rbromilo@e5r10p11.jnb.42.fr>
2016-09-06 10:32:40 +0200

    fixed prompt, put slash, stop duplication or line

--- srcs/line_editing/ft_init_terminal.c
+++ srcs/line_editing/ft_init_terminal.c
@@ -6,7 +6,7 @@
 /*   By: tmack <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/28 16:48:49 by tmack             #+#    #+#             */
-/*   Updated: 2016/09/06 10:12:26 by kgani            ###   ########.fr       */
+/*   Updated: 2016/09/06 10:16:04 by kgani            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,7 +66,7 @@ int				init_terminal_data(t_shell *s)
 		err(0, term);
 	s->new_term = s->default_term;
 	s->new_term.c_lflag |= ISIG;
-	s->new_term.c_lflag &= ~(ICANON | ECHO);
+	s->new_term.c_lflag &= ~(ICANON | ECHO | ISIG);
 	s->new_term.c_cc[VMIN] = 1;
 	s->new_term.c_cc[VTIME] = 0;
 	tcsetattr(STDIN_FILENO, TCSADRAIN, &s->new_term);
